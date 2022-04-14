28e1e1ed4956cb9187ab9f98f9a2efcfc71296b9
ngotiendat <datmau20@gmail.com>
2022-04-14 12:18:42 +0700

    haha

---
+++ main.js
@@ -0,0 +1,15 @@
+var obj = {};
+
+var dongvat = [
+  {
+    ten: gaugau,
+    sua() {
+      alert("gauagu");
+    },
+  },
+];
+
+obj[dongvat.tencho] = [dongvat.sua];
+console.log(obj);
+
+console.log;
